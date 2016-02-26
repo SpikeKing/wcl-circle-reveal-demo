c7f51403ac706e0afe0647438cc2251057ee0207
spike <morndragon@126.com>
2016-02-26 10:12:07 +0800

    添加页面和基础配置

--- app/src/main/java/me/chunyu/spike/wcl_circle_reveal_demo/MainActivity.java
+++ app/src/main/java/me/chunyu/spike/wcl_circle_reveal_demo/MainActivity.java
@@ -1,31 +1,31 @@
 package me.chunyu.spike.wcl_circle_reveal_demo;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.fab) FloatingActionButton mFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -48,5 +48,16 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // Fab的跳转事件
+    public void startOtherActivity(View view) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ActivityOptions options =
+                    ActivityOptions.makeSceneTransitionAnimation(this, mFab, mFab.getTransitionName());
+            startActivity(new Intent(this, OtherActivity.class), options.toBundle());
+        } else {
+            startActivity(new Intent(this, OtherActivity.class));
+        }
     }
 }
---
+++ app/src/main/java/me/chunyu/spike/wcl_circle_reveal_demo/OtherActivity.java
@@ -0,0 +1,17 @@
+package me.chunyu.spike.wcl_circle_reveal_demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 跳转的Activity
+ * <p>
+ * Created by wangchenlong on 16/2/26.
+ */
+public class OtherActivity extends AppCompatActivity {
+    @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other);
+    }
+}
