f356ba166f1a7106436167eb44f966eeb8b81093
spike <morndragon@126.com>
2016-02-26 15:47:45 +0800

    完成全部动画效果

---
+++ app/src/main/java/me/chunyu/spike/wcl_circle_reveal_demo/GuiUtils.java
@@ -0,0 +1,85 @@
+package me.chunyu.spike.wcl_circle_reveal_demo;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.ColorRes;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
+/**
+ * 动画效果
+ * <p>
+ * Created by wangchenlong on 16/2/26.
+ */
+public class GuiUtils {
+    public interface OnRevealAnimationListener {
+        void onRevealHide();
+
+        void onRevealShow();
+    }
+
+    // 圆圈爆炸效果显示
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public static void animateRevealShow(
+            final Context context, final View view,
+            final int startRadius, @ColorRes int color,
+            OnRevealAnimationListener listener) {
+        int cx = (view.getLeft() + view.getRight()) / 2;
+        int cy = (view.getTop() + view.getBottom()) / 2;
+
+        float finalRadius = (float) Math.hypot(view.getWidth(), view.getHeight());
+
+        // 设置圆形显示动画
+        Animator anim = ViewAnimationUtils.createCircularReveal(view, cx, cy, startRadius, finalRadius);
+        anim.setDuration(300);
+        anim.setInterpolator(new AccelerateDecelerateInterpolator());
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                view.setVisibility(View.VISIBLE);
+                listener.onRevealShow();
+            }
+
+            @Override public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+                view.setBackgroundColor(ContextCompat.getColor(context, color));
+            }
+        });
+
+        anim.start();
+    }
+
+    // 圆圈退出效果
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public static void animateRevealHide(
+            final Context context, final View view,
+            final int finalRadius, @ColorRes int color,
+            OnRevealAnimationListener listener
+    ) {
+        int cx = (view.getLeft() + view.getRight()) / 2;
+        int cy = (view.getTop() + view.getBottom()) / 2;
+        int initialRadius = view.getWidth();
+        // 与入场动画的区别就是圆圈起始和终止的半径相反
+        Animator anim = ViewAnimationUtils.createCircularReveal(view, cx, cy, initialRadius, finalRadius);
+        anim.setDuration(300);
+        anim.setInterpolator(new AccelerateDecelerateInterpolator());
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+                view.setBackgroundColor(ContextCompat.getColor(context, color));
+            }
+
+            @Override public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                listener.onRevealHide();
+                view.setVisibility(View.INVISIBLE);
+            }
+        });
+        anim.start();
+    }
+}
--- app/src/main/java/me/chunyu/spike/wcl_circle_reveal_demo/OtherActivity.java
+++ app/src/main/java/me/chunyu/spike/wcl_circle_reveal_demo/OtherActivity.java
@@ -1,8 +1,24 @@
 package me.chunyu.spike.wcl_circle_reveal_demo;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.transition.Fade;
+import android.transition.Transition;
+import android.transition.TransitionInflater;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * 跳转的Activity
@@ -10,8 +26,121 @@ import android.support.v7.app.AppCompatActivity;
  * Created by wangchenlong on 16/2/26.
  */
 public class OtherActivity extends AppCompatActivity {
+
+    @Bind(R.id.other_fab_circle) FloatingActionButton mFabCircle;
+    @Bind(R.id.other_v_container) View mVContainer;
+    @Bind(R.id.other_iv_close) ImageView mIvClose;
+    @Bind(R.id.other_rl_container) RelativeLayout mRlContainer;
+
     @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+        ButterKnife.bind(this);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setupEnterAnimation(); // 入场动画
+            setupExitAnimation(); // 退场动画
+        } else {
+            initViews();
+        }
+    }
+
+    // 入场动画
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void setupEnterAnimation() {
+        Transition transition = TransitionInflater.from(this)
+                .inflateTransition(R.transition.arc_motion);
+        getWindow().setSharedElementEnterTransition(transition);
+        transition.addListener(new Transition.TransitionListener() {
+            @Override public void onTransitionStart(Transition transition) {
+
+            }
+
+            @Override public void onTransitionEnd(Transition transition) {
+                transition.removeListener(this);
+                animateRevealShow();
+            }
+
+            @Override public void onTransitionCancel(Transition transition) {
+
+            }
+
+            @Override public void onTransitionPause(Transition transition) {
+
+            }
+
+            @Override public void onTransitionResume(Transition transition) {
+
+            }
+        });
+    }
+
+    // 动画展示
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void animateRevealShow() {
+        GuiUtils.animateRevealShow(
+                this, mRlContainer,
+                mFabCircle.getWidth() / 2, R.color.colorAccent,
+                new GuiUtils.OnRevealAnimationListener() {
+                    @Override public void onRevealHide() {
+
+                    }
+
+                    @Override public void onRevealShow() {
+                        initViews();
+                    }
+                });
+    }
+
+    // 初始化视图
+    private void initViews() {
+        new Handler(Looper.getMainLooper()).post(() -> {
+            Animation animation = AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
+            animation.setDuration(300);
+            mVContainer.startAnimation(animation);
+            mIvClose.setAnimation(animation);
+            mVContainer.setVisibility(View.VISIBLE);
+            mIvClose.setVisibility(View.VISIBLE);
+        });
+    }
+
+    // 退出动画
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void setupExitAnimation() {
+        Fade fade = new Fade();
+        getWindow().setReturnTransition(fade);
+        fade.setDuration(300);
+    }
+
+    // 退出按钮
+    public void backActivity(View view) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            onBackPressed();
+        } else {
+            defaultBackPressed();
+        }
+    }
+
+    // 退出事件
+    @Override public void onBackPressed() {
+        GuiUtils.animateRevealHide(
+                this, mRlContainer,
+                mFabCircle.getWidth() / 2, R.color.colorAccent,
+                new GuiUtils.OnRevealAnimationListener() {
+                    @Override
+                    public void onRevealHide() {
+                        defaultBackPressed();
+                    }
+
+                    @Override
+                    public void onRevealShow() {
+
+                    }
+                });
+    }
+
+    // 默认回退
+    private void defaultBackPressed() {
+        super.onBackPressed();
     }
 }
